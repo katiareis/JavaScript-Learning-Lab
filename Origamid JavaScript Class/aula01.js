a2fe00fb57ba7ef7bcbf23892523611dc2dd51df
Katia Reis <62106987+katiareis@users.noreply.github.com>
2021-06-28 18:43:34 -0300

    JavaScript para Iniciantes - Origamid
    Curso de JavaScript para iniciantes da Origamid.
    Aula 01 - Variáveis

---
+++ Origamid JavaScript Class/aula01.js
@@ -0,0 +1,21 @@
+var nome = 'Kátia';
+var idade = 28;
+var pussuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade); // Imprime no console o que estiver entre os parênteses
+
+// Evita repetições - Conceito Dry
+var preco = 20;
+var totalComprado = 5;
+var preçoTotal = preco * totalComprado;
+
+// Podem ser declaradas diversar variáveis no mesmo momento, utilizando a vírgula.
+
+var nome = "Ana",
+    sobrenome = "Silva",
+    idade = 25;
+
+// É possível declarar a variável sem valor, para declarar o valor posteriormente
+var pais;
+
+
---
+++ Origamid JavaScript Class/exercicio01.js
@@ -0,0 +1,33 @@
+/* Exercícios de JavaScript - Variáveis */
+/* Backup Info: https://www.w3schools.com/js/js_variables.asp */
+
+
+// Declarar uma variável com o seu nome
+var nome = "Kátia";
+
+// Declarar uma variável com a sua idade
+var idade = 34;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comida = "pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+var pizza;
+var altura;
+var animalDeEstimacao;
+var anoDeNascimento;
+var carro;
+
+//ou
+
+var time = 'Palmeiras',
+    professor,
+    pais,
+    endereco,
+    rua;
+
+console.log(nome, idade, comidaFavorita)
