d0d47cbb8a2ebef3ed4dd0c6b11d776e84fd9894
Katia Reis <62106987+katiareis@users.noreply.github.com>
2022-01-05 21:53:28 -0300

    Add new files
    Learning more about Javascript.

---
+++ Origamid JavaScript Class/Tipos de Dados/aula02.js
@@ -0,0 +1,59 @@
+/* Aula e Exercícios da aula 02 - JS Origamid */
+
+/* Backup Info: https://www.w3schools.com/js/js_datatypes.asp */
+
+// Tipos de dados
+
+var nome = "André"; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol(); // Symbol
+var novoObjeto = {}; // Object
+
+// Verificar o tipo de Dado
+
+var nome = "André";
+console.log(typeof nome);
+// retorna string
+
+var idade = 28; // Number
+
+/* *************************************************************************************** */
+
+/* Exercícios de JavaScript - Tipos de Dados */
+
+// Declare uma variável contendo uma string
+var nome = "Meu nome é Kátia";
+
+// Declare uma variável contendo um número dentro de uma string
+var idade = "Eu tenho 35 anos";
+var ano = "2018";
+// Declare uma variável com a sua idade
+var age = 35;
+
+// Declare duas variáveis, uma com seu nome
+var name = "Katia";
+
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = "Reis";
+
+var nomeCompleto = nome + " " + sobrenome;
+//ou
+var fullname = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
+console.log(fullname);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
+
+//ou
+
+var verificarTipo = typeof nomeCompleto;
+
+console.log(verificarTipo);
---
+++ Origamid JavaScript Class/variaveis/aula01.js
@@ -0,0 +1,59 @@
+/* Aula e Exercícios da aula 01 - JS Origamid */
+
+/* Backup Info: https://www.w3schools.com/js/js_variables.asp */
+
+// Variáveis
+
+var nome = "Kátia";
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade); // Imprime no console o que estiver entre os parênteses
+
+// Evita repetições - Conceito Dry
+var preco = 20;
+var totalComprado = 5;
+var preçoTotal = preco * totalComprado;
+
+// Podem ser declaradas diversar variáveis no mesmo momento, utilizando a vírgula.
+
+var nome = "Ana",
+  sobrenome = "Silva",
+  idade = 25;
+
+// É possível declarar a variável sem valor, para declarar o valor posteriormente
+var pais;
+
+/* *************************************************************************************** */
+
+/* Exercícios de JavaScript - Variáveis */
+
+// Declarar uma variável com o seu nome
+var nome = "Kátia";
+
+// Declarar uma variável com a sua idade
+var idade = 34;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comida = "pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+var pizza;
+var altura;
+var animalDeEstimacao;
+var anoDeNascimento;
+var carro;
+
+//ou
+
+var time = "Palmeiras",
+  professor,
+  pais,
+  endereco,
+  rua;
+
+console.log(nome, idade, comidaFavorita, rua);
