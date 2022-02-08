c7d83a735b64884dbb3255d2da399a6b3d880e87
Katia Reis <62106987+katiareis@users.noreply.github.com>
2022-02-08 16:05:12 -0300

    Update forecast.js
    Create first part pf the App. Connects the App with the API using a key. Add a async JS code to create a promise of Jason data.

---
+++ udemy-ninja/projects/weather-app/scripts/forecast.js
@@ -0,0 +1,18 @@
+/* AccuWeather API - https://developer.accuweather.com/ */
+
+const key = "	xvpOBAAypFh84YftzPvUCh8ZM80gbYIG";
+
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("manchester")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
