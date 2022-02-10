6a1f0403367aafbed23b10825e1935e1aba3c092
Katia Reis <62106987+katiareis@users.noreply.github.com>
2022-02-10 00:19:21 -0300

    Update app.js
    Calls updateUI function, make the async request, gets data from API and output it to the UI with DOM manipulation.

--- udemy-ninja/projects/weather-app/scripts/app.js
+++ udemy-ninja/projects/weather-app/scripts/app.js
@@ -1,14 +1,31 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+
+  // Update details template
+  details.innerHTML = `<h5 class="my-3">${cityDetails.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
+  </div>`;
+
+  //remove display none class if weather
+  if (card.classList.contains("display-none")) {
+    card.classList.remove("display-none");
+  }
+};
 
 const updateCity = async (city) => {
   const cityDetails = await getCity(city);
   const weather = await getWeather(cityDetails.Key);
 
   //returns an object
-  return {
-    cityDetails: cityDetails,
-    weather: weather,
-  };
+  return { cityDetails, weather };
 };
 
 cityForm.addEventListener("submit", (e) => {
@@ -21,6 +38,6 @@ cityForm.addEventListener("submit", (e) => {
 
   //update the UI with the new city
   updateCity(city)
-    .then((data) => console.log(data))
-    .catch((err) => console.error(err));
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 });
